2fcab1b40cd53de6848207299ce833b421ce2c40
Sushil Subedi <sushilsubedi32@gmail.com>
2019-10-27 00:39:54 +0545

    removed all

--- Menu.php
+++
@@ -1,6 +0,0 @@
-<div id="container-menu">
-   <a href="index.php">Home</a>
-   <a href="about.php">About</a>
-   <a href="contact.php">Contact</a>
-
-</div>
--- form.php
+++
@@ -1,7 +0,0 @@
-<form action="login.php" method="POST">
-	username: <input name="user"/><br>
-	password: <input name="pass"/><br>
-
-	<input type="submit"/>
-
-</form>
